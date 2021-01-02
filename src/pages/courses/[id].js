58ba91b145e4e7b4b73fdc79d048d9e3af143c5e
Luthfi Aziz Nugraha <nugrahaazizluthfi@yahoo.com>
2021-01-02 22:26:50 +0700

    add instructor

--- src/pages/courses/[id].js
+++ src/pages/courses/[id].js
@@ -173,6 +173,30 @@ const DetailCourses = ({ data }) => {
                 <div className="w-full text-center py-12">No Chapter Found</div>
               )}
             </section>
+
+            <section className="mt-10">
+              <h6 className="font-medium text-gray-900 text-2xl mb-4">
+                Our <span className="text-teal-500">Instructor</span>
+              </h6>
+              <div className="flex items-center">
+                <img
+                  src={
+                    data?.mentor?.profile ??
+                    'http://media.bwamicro.com/images/1593527439774.png'
+                  }
+                  alt={data?.mentor?.name ?? ''}
+                  className="w-20 rounded-full overflow-hidden object-cover"
+                />
+                <div className="ml-4">
+                  <h2 className="text-lg text-gray-900">
+                    {data?.mentor?.name ?? "Mentor's name"}
+                  </h2>
+                  <h3 className="text-sm text-gray-600">
+                    {data?.mentor?.profession}
+                  </h3>
+                </div>
+              </div>
+            </section>
           </div>
         </div>
       </section>
